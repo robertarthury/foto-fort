9eb2ca34904352ace3c2f22710056bac0f833747
robertarthury <robertarthury@gmail.com>
2022-06-22 23:25:01 -0400

    contact

---
+++ src/components/Contact/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, name: e.target.value })
+    }
+
+     
+    return (
+        <section>
+            <h1>Contact Me</h1>
+            <form id='contact-form'>
+                <div>
+                    <label htmlFor='name' name='name'>Name:</label>
+                        <input type='text' defaultValue={name} name='name'/>
+                </div>
+                <div>
+                    <label htmlFor='email'>Email Address:</label>
+                        <input type='email' defaultValue={email} name='email' onChange={handleChange}  />
+                </div>
+                <div>
+                    <label htmlFor='message'>Message:</label>
+                        <textarea name='message' defaultValue={message} onChange={handleChange} rows='5' />
+                </div>
+                <button type='submit'>Submit</button>
+            </form>
+        </section>
+    );
+}
+
+export default ContactForm;
